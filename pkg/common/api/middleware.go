dc008e8324ebe25b61233066d170079af3cb01ca
iwanlaudin <iwanlaudin01@gmail.com>
2024-10-03 11:38:38 +0700

    add middleware api versioning

--- pkg/common/api/middleware.go
+++ pkg/common/api/middleware.go
@@ -11,6 +11,19 @@ import (
 	"golang.org/x/time/rate"
 )
 
+type contextKey string
+
+const apiVersionKey contextKey = "api.version"
+
+func ApiVersionCtx(version string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			r = r.WithContext(context.WithValue(r.Context(), apiVersionKey, version))
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
 func ErrorLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
